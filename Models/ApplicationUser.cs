64b045f346c4bec616b98c06516e687ea0c09d47
tonykri <tonykristaki@gmail.com>
2024-01-03 21:30:39 +0200

    Add models

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabProjectsPortal.Models
+{
+    public class ApplicationUser
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public byte[] PasswordHash { get; set; } = null!;
+        public byte[] PasswordSalt { get; set; } = null!;
+        [Editable(false)]
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+        public ICollection<Contact> UserSentContacts { get; set; } = new List<Contact>();
+        public ICollection<Contact> UserReceivedContacts { get; set; } = new List<Contact>();
+        public ICollection<Conversation> Conversations { get; set; } = new List<Conversation>();
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+        public ICollection<Hobby> Hobbies { get; set; } = new List<Hobby>();
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabProjectsPortal.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = null!;
+    }
+}
