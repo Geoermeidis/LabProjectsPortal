79ed9099f4b716776e78fa83ab5fb3e2abb62d43
Geoermeidis <75807347+Geoermeidis@users.noreply.github.com>
2024-01-18 21:42:01 +0200

    Added messages index function for conversation

--- Models/PartialClasses.cs
+++ Models/PartialClasses.cs
@@ -16,4 +16,35 @@ namespace LabProjectsPortal.Models
             }
         }
     }
+
+    public partial class Message {
+        [DisplayName("Sent")]
+        public string SentAtDifference
+        {
+            get
+            {
+                var timeDifference = DateTimeOffset.Now - SentAt;
+
+                double seconds = timeDifference.Seconds;
+                double minutes = timeDifference.TotalMinutes;
+                double hours = timeDifference.TotalHours;
+                double days = timeDifference.Days;
+
+                // Construct the formatted string
+                string time;
+                if (minutes < 1 && hours == 0 && days == 0) 
+                    time = string.Format("{0} seconds ago", Math.Floor(seconds));
+                else if (minutes > 1 && hours == 0 && days == 0)
+                    time = string.Format("{0} minutes ago", Math.Floor(minutes));
+                else if (hours <= 12 && days == 0)
+                    time = string.Format("{0} hours ago", Math.Floor(hours)); 
+                else if(days==0)
+                    time = string.Format("{0}:{1}", SentAt.Hour, SentAt.Minute);
+                else
+                    time = string.Format("{0}", SentAt.Date.ToShortDateString());
+
+                return time;
+            }
+        }
+    }
 }
